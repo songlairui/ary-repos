c54ff07e2cf4f4f75022433c636ddfbfa2b21096
songlairui <songlairui@126.com>
2019-09-23 02:24:30 +0800

    feat(remove): unused

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ReposModule } from './repos/repos.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot(), ReposModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
